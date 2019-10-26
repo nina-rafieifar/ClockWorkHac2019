205c7e01fc87bf4652921c7663fea0ca929f3843
Nina Rafieifar <nina.rafieifar@gmail.com>
2019-10-26 23:42:00 +0100

    Will do the rest tomorrow

--- OutReach/outreach.ui/src/App.tsx
+++ OutReach/outreach.ui/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import { Registration } from "./components/registration";
+import { Verification } from "./components/verification";
+import { Container } from "reactstrap";
 
 class App extends React.Component<any> {
   constructor(props: any) {
@@ -10,10 +12,18 @@ class App extends React.Component<any> {
   public render(): React.ReactNode {
     return (
       <div className="App">
-        <header>
-          <div className="App-logo"></div>
-        </header>
-        <Registration></Registration>
+        <div className="Form">
+          <div className="FormSubset">
+            <header>{/* <div className="App-logo"></div> */}</header>
+            <Container>
+              <div>
+                <h1 className="formTitle">Welcome to ReachOut</h1>
+              </div>
+              <Registration></Registration>
+              <Verification patientForename="nina" patientSurname="raf" phoneNumber="448629639126"></Verification>
+            </Container>
+          </div>
+        </div>
       </div>
     );
   }
