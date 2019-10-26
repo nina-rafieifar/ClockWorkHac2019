2c64afb9dcab28a071cd4082d83890581795f871
Nina Rafieifar <nina.rafieifar@gmail.com>
2019-10-26 19:45:42 +0100

    little changes to ui

--- OutReach/outreach.ui/src/App.tsx
+++ OutReach/outreach.ui/src/App.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Registration } from './components/registration';
+import React from "react";
+import "./App.css";
+import { Registration } from "./components/registration";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Registration></Registration>
-    </div>
-  );
+interface IAppState {
+  /**
+   * Patient Forename
+   */
+  offset: any;
 }
 
+class App extends React.Component<any, IAppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      offset: 0
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("scroll", this.parallaxShift);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.parallaxShift);
+  }
+  parallaxShift = () => {
+    this.setState({
+      offset: window.pageYOffset
+    });
+  };
+
+  public render(): React.ReactNode {
+    const { offset } = this.state;
+    return (
+      <div className="App">
+        <header className="header-background" style={{ backgroundPositionY: offset }}>
+          <section className="info-container" style={{ bottom: offset / 2 }}>
+            <div className="App-logo"></div>
+          </section>
+        </header>
+        <Registration></Registration>
+      </div>
+    );
+  }
+}
 export default App;
