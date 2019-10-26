dfc8fd72a718391185640cfe79759ce5699f3fe9
Oliver Bullock <oliver.bullock@emishealth.com>
2019-10-26 16:52:42 +0100

    Created new API

---
+++ OutReach/OutReach.API/Models/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutReach.API.Models
+{
+	public class Person
+	{
+		public string Forename { get; set; }
+		public string Surname { get; set; }
+		public string MobilePhoneNumber { get; set; }
+	}
+
+	public class Patient : Person
+	{
+		public DateTime DateOfBirth { get; set; }
+	}
+
+	public class SupportNetworkContact : Person
+	{
+		public string Relationship { get; set; }
+	}
+
+	public class PatientProfile
+	{
+		public Patient Patient { get; set; }
+	}
+}
